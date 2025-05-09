0160b2b2669ee148b69b0aa2765c08f3a483d31c
YogeshFirake <firakeyogesh@gmail.com>
2025-05-09 13:39:19 +0530

    add heading

--- src/componants/Hero.jsx
+++ src/componants/Hero.jsx
@@ -8,8 +8,8 @@ const Hero = () => {
                 <div className="w-full lg:w-1/2">
                     <div className="flex flex-col item-center lg:item-start">
                         <h1
-                        
-                        className="pb-16 text-5xl font-Bold tracking-tight lg:mg-16 lg:text-5xl" >Yogesh Firake</h1>
+                      
+                        className="pb-16 text-5xl font-Bold tracking-tight lg:mg-16 lg:text-5xl" >Yogesh Firake   Mern stack developer</h1>
                         <span className="pg-gradient-to-r from-pink-300 via-slate-500 to-purplr-500 bg-clip-text text-4xl tracking-tight text-transperent">Front End Developer</span>
                         <p className="my-2 max-w-xl py-6 font-light text-justify tracking-tighter">{HERO_CONTENT}</p>
                     </div>
