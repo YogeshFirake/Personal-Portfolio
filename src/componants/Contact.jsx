4620b80ad9b7c0787fb4302911040d9ecef102f3
YogeshFirake <162883017+YogeshFirake@users.noreply.github.com>
2025-06-18 18:19:37 +0530

    Add existing file

--- src/componants/Contact.jsx
+++ src/componants/Contact.jsx
@@ -8,6 +8,8 @@ const contact = () => {
         <p className="my-4">{ CONTACT.address}</p>
         <p className="my-4">{CONTACT.phoneNo}</p>
         <a href="#" className= "border-b">{CONTACT.email}</a>
+        <br/>
+        <a href="#" className= "border-b">{CONTACT.Linkedin} </a>
 
         </div>      
     </div>
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,11 +1,11 @@
+import { LiaLinkedin } from "react-icons/lia";
 import project1 from "../assets/projects/project-1.png";
 import project2 from "../assets/projects/project-2.png";
 import project3 from "../assets/projects/project-3.png";
 
-
 export const HERO_CONTENT = `I am a passionate front-end UI developer with a knack for crafting robust and scalable web applications. With 1+ years of hands-on experience, I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
 
-export const ABOUT_TEXT = `I am a dedicated and versatile frontend developer and Reactjs with a passion for creating efficient and user-friendly web applications. With 1.3 years of professional experience, I have worked with a variety of technologies, including React, React.js, Javascript, Tailwind Css, Bootstrap, Htmlm, Git & GitHub. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
+export const ABOUT_TEXT = `I am a dedicated and versatile frontend developer and Reactjs with a passion for creating efficient and user-friendly web applications. With 1+ years of professional experience, I have worked with a variety of technologies, including React, React.js, Javascript, Tailwind Css, Bootstrap, Htmlm, Git & GitHub. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
 
 export const EXPERIENCES = [
   {
@@ -13,7 +13,7 @@ export const EXPERIENCES = [
     role: "Front End Developer",
     company: "Rego Digital Solutions",
     description: `Led a team in developing and maintaining web evelopment using JavaScript, React.js, and Bootstrap. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
-    technologies: [ "React.js", "Bootstrap", "Html", "Css", "Javascript"],
+    technologies: ["React.js", "Bootstrap", "Html", "Css", "Javascript"],
   },
   {
     year: "Jan-2021 = Nov-2023",
@@ -29,7 +29,6 @@ export const EXPERIENCES = [
     description: `I use my knowledge of company products, services, and policies to assist callers with inquiries, complaints, or problems. They speak with customers, listen to them gain a better understanding of their needs, and offer possible solutions.`,
     technologies: ["Oracle", "SAP", "MS-Office"],
   },
-  
 ];
 
 export const PROJECTS = [
@@ -45,19 +44,20 @@ export const PROJECTS = [
     image: project2,
     description:
       "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
-    technologies: ["HTML", "CSS", "React", ],
+    technologies: ["HTML", "CSS", "React"],
   },
   {
     title: "Appvilla ",
     image: project3,
     description:
       "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
-    technologies: ["HTML", "CSS", ],
+    technologies: ["HTML", "CSS"],
   },
-  ];
+];
 
-export const CONTACT = {  
-  address: "At Po. Engaon Tal. Bodwad Dist. Jalgaon - 425310",
-  phoneNo: "+91 7888 200 887 ",
-  email: "firakeyogesh@gmail.com",
+export const CONTACT = {
+  address: " At Po. Engaon Tal. Bodwad Dist. Jalgaon - 425310",
+  phoneNo: " +91 7888 200 887 ",
+  email: " firakeyogesh@gmail.com",
+  Linkedin: " https://www.linkedin.com/in/yogesh-firake-3567771ba",
 };
